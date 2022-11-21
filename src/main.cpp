6e00874731dcafca4aca9cee6e791c7ceec0cbc2
EdySurya <72597608+EdySurya@users.noreply.github.com>
2022-11-21 17:35:37 +0800

    new

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <WiFiMulti.h>
+#include <WiFi.h>
 #include <HTTPClient.h>
 #include <Update.h>
 #include <LittleFS.h>
@@ -12,7 +12,6 @@ const char *password = "smarthotelbali";
 const char *urlJson = "https://raw.githubusercontent.com/EdySurya/HTTP_OTA_ESP/LittleFS/data/CurrentVersion.json";
 const char *url = "https://raw.githubusercontent.com/EdySurya/HTTP_OTA_ESP/LittleFS/src/Update/littlefs.bin";
 const char *urlFirm = "https://raw.githubusercontent.com/EdySurya/HTTP_OTA_ESP/LittleFS/src/Update/firmware.bin";
-WiFiMulti wifiMulti;
 
 String currentFSVers = "0.1";
 String currentFirmVers = "0.1";
@@ -48,120 +47,114 @@ void updateFirmware(uint8_t *data, size_t len)
 }
 void UpdateFS()
 {
-  if ((wifiMulti.run() == WL_CONNECTED))
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+  // configure server and url
+  http.begin(url);
+  // http.begin("192.168.1.12", 80, "/test.html");
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+  if (httpCode > 0)
   {
-    HTTPClient http;
-    Serial.print("[HTTP] begin...\n");
-    // configure server and url
-    http.begin(url);
-    // http.begin("192.168.1.12", 80, "/test.html");
-    Serial.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
-    if (httpCode > 0)
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    // file found at server
+    Update.begin(UPDATE_SIZE_UNKNOWN, U_SPIFFS, -1, LOW, NULL);
+    if (httpCode == HTTP_CODE_OK)
     {
-      // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-      // file found at server
-      Update.begin(UPDATE_SIZE_UNKNOWN, U_SPIFFS, -1, LOW, NULL);
-      if (httpCode == HTTP_CODE_OK)
+      // get length of document (is -1 when Server sends no Content-Length header)
+      totalLength = http.getSize();
+      int len = totalLength;
+      // create buffer for read
+      uint8_t buff[128] = {0};
+      // get tcp stream
+      WiFiClient *stream = http.getStreamPtr();
+      // read all data from server
+      while (http.connected() && (len > 0 || len == -1))
       {
-        // get length of document (is -1 when Server sends no Content-Length header)
-        totalLength = http.getSize();
-        int len = totalLength;
-        // create buffer for read
-        uint8_t buff[128] = {0};
-        // get tcp stream
-        WiFiClient *stream = http.getStreamPtr();
-        // read all data from server
-        while (http.connected() && (len > 0 || len == -1))
+        // get available data size
+        size_t size = stream->available();
+        if (size)
         {
-          // get available data size
-          size_t size = stream->available();
-          if (size)
+          // read up to 128 byte
+          int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+          // write it to Serial
+          updateFirmware(buff, c);
+          // Serial.write(buff, c);
+          if (len > 0)
           {
-            // read up to 128 byte
-            int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
-            // write it to Serial
-            updateFirmware(buff, c);
-            // Serial.write(buff, c);
-            if (len > 0)
-            {
-              len -= c;
-            }
+            len -= c;
           }
-          delay(1);
         }
-        Serial.println();
-        Serial.print("[HTTP] connection closed or file end.\n");
+        delay(1);
       }
+      Serial.println();
+      Serial.print("[HTTP] connection closed or file end.\n");
     }
-    else
-    {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
-    http.end();
   }
+  else
+  {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
 }
 
 void UpdateFirm()
 {
-  if ((wifiMulti.run() == WL_CONNECTED))
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+  // configure server and url
+  http.begin(urlFirm);
+  // http.begin("192.168.1.12", 80, "/test.html");
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+  if (httpCode > 0)
   {
-    HTTPClient http;
-    Serial.print("[HTTP] begin...\n");
-    // configure server and url
-    http.begin(urlFirm);
-    // http.begin("192.168.1.12", 80, "/test.html");
-    Serial.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
-    if (httpCode > 0)
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    // file found at server
+    Update.begin(UPDATE_SIZE_UNKNOWN, U_FLASH, -1, LOW, NULL);
+    if (httpCode == HTTP_CODE_OK)
     {
-      // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-      // file found at server
-      Update.begin(UPDATE_SIZE_UNKNOWN, U_FLASH, -1, LOW, NULL);
-      if (httpCode == HTTP_CODE_OK)
+      // get length of document (is -1 when Server sends no Content-Length header)
+      totalLength = http.getSize();
+      int len = totalLength;
+      // create buffer for read
+      uint8_t buff[128] = {0};
+      // get tcp stream
+      WiFiClient *stream = http.getStreamPtr();
+      // read all data from server
+      while (http.connected() && (len > 0 || len == -1))
       {
-        // get length of document (is -1 when Server sends no Content-Length header)
-        totalLength = http.getSize();
-        int len = totalLength;
-        // create buffer for read
-        uint8_t buff[128] = {0};
-        // get tcp stream
-        WiFiClient *stream = http.getStreamPtr();
-        // read all data from server
-        while (http.connected() && (len > 0 || len == -1))
+        // get available data size
+        size_t size = stream->available();
+        if (size)
         {
-          // get available data size
-          size_t size = stream->available();
-          if (size)
+          // read up to 128 byte
+          int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+          // write it to Serial
+          updateFirmware(buff, c);
+          // Serial.write(buff, c);
+          if (len > 0)
           {
-            // read up to 128 byte
-            int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
-            // write it to Serial
-            updateFirmware(buff, c);
-            // Serial.write(buff, c);
-            if (len > 0)
-            {
-              len -= c;
-            }
+            len -= c;
           }
-          delay(1);
         }
-        Serial.println();
-        Serial.print("[HTTP] connection closed or file end.\n");
-        Serial.println("Rebooting...");
-        ESP.restart();
+        delay(1);
       }
+      Serial.println();
+      Serial.print("[HTTP] connection closed or file end.\n");
+      Serial.println("Rebooting...");
+      ESP.restart();
     }
-    else
-    {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
-    http.end();
   }
+  else
+  {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
 }
 
 void readFile(const char *path, const char *mode)
@@ -178,13 +171,14 @@ void setup()
 {
   Serial.begin(115200);
   // delay(4000);
-  // WiFi.begin(ssid, password);
-  wifiMulti.addAP(ssid, password);
+  WiFi.begin(ssid, password);
+  // wifiMulti.addAP(ssid, password);
 
-  while (wifiMulti.run() != WL_CONNECTED)
+  Serial.println("Connecting to WiFi..");
+  while (WiFi.status() != WL_CONNECTED)
   {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.print(".");
+    delay(250);
   }
   Serial.println("Connected to the WiFi network");
   initLittleFS();
@@ -216,53 +210,51 @@ void setup()
 }
 void loop()
 {
-  if ((wifiMulti.run() == WL_CONNECTED))
+  Serial.println("Firmware Current Version :" + currentFirmVers);
+  Serial.println("FS Current Version :" + currentFSVers);
+  DynamicJsonDocument doc(64);
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+  // configure server and url
+  http.useHTTP10(true);
+  http.begin(urlJson);
+  // http.begin("192.168.1.12", 80, "/test.html");
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+  if (httpCode > 0)
   {
-    Serial.println("Firmware Current Version :" + currentFirmVers);
-    Serial.println("FS Current Version :" + currentFSVers);
-    DynamicJsonDocument doc(2048);
-    HTTPClient http;
-    Serial.print("[HTTP] begin...\n");
-    // configure server and url
-    http.useHTTP10(true);
-    http.begin(urlJson);
-    // http.begin("192.168.1.12", 80, "/test.html");
-    Serial.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
-    if (httpCode > 0)
+    deserializeJson(doc, http.getStream());
+    Serial.print("FS Version : ");
+    Serial.println(doc["FSVers"].as<String>());
+    Serial.print("Firmware Version : ");
+    Serial.println(doc["FirmVers"].as<String>());
+    if (doc["FSVers"].as<String>() != currentFSVers)
     {
-      deserializeJson(doc, http.getStream());
-      Serial.print("FS Version : ");
-      Serial.println(doc["FSVers"].as<String>());
-      Serial.print("Firmware Version : ");
-      Serial.println(doc["FirmVers"].as<String>());
-      if (doc["FSVers"].as<String>() != currentFSVers)
-      {
-        currentFSVers = doc["FSVers"].as<String>();
-        Serial.println("Memulai Update Versi baru dari Little FS");
-        UpdateFS();
-        currentFSVers = docVer["FSVers"].as<String>();
-      }
-      else if (doc["FirmVers"].as<String>() != currentFirmVers)
-      {
-        currentFirmVers = doc["FirmVers"].as<String>();
-        Serial.println("Memulai Update Versi baru Firmware");
-        UpdateFirm();
-        currentFirmVers = docVer["FirmVers"].as<String>();
-      }
-      readFile("/LampState.json", "r");
-      readFile("/text.txt", "r");
-      // readFile("/index.html", "r");
-      // readFile("/script.js", "r");
-      // readFile("/style.css", "r");
-      readFile("/CurrentVersion.json", "r");
+      currentFSVers = doc["FSVers"].as<String>();
+      Serial.println("Memulai Update Versi baru dari Little FS");
+      UpdateFS();
+      // currentFSVers = docVer["FSVers"].as<String>();
     }
-    else
+    else if (doc["FirmVers"].as<String>() != currentFirmVers)
     {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      currentFirmVers = doc["FirmVers"].as<String>();
+      Serial.println("Memulai Update Versi baru Firmware");
+      UpdateFirm();
+      // currentFirmVers = docVer["FirmVers"].as<String>();
     }
-    http.end();
+    readFile("/LampState.json", "r");
+    readFile("/text.txt", "r");
+    // readFile("/index.html", "r");
+    // readFile("/script.js", "r");
+    // readFile("/style.css", "r");
+    readFile("/CurrentVersion.json", "r");
   }
-  delay(2000);
+  else
+  {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+
+  delay(250);
 }
